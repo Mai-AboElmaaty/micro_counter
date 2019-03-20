efde2b7d5979e0b4f7745ed101cb70c7dcbde81e
nadakoga <nadokhojah@hotmail.com>
2019-03-20 07:41:07 +0200

    Add files via upload

--- Functions/write_port.c
+++ Functions/write_port.c
@@ -5,26 +5,68 @@
 void DIO_WritePort(uint8_t port_index, uint8_t pins_mask, Dio_levelType pins_level){
 	if (port_index == 0) // Port A
 	{
-	   GPIO_PORTA_DATA_R & pins_mask =  pins_level;
+		if (pins_level==STD_HIGH)
+		{
+			 GPIO_PORTA_DATA_R |= pins_mask;
+		}
+		else if(pins_level==STD_LOW)
+			{
+			GPIO_PORTA_DATA_R &= ~pins_mask;
+		  }
 	}
 	else if (port_index == 1) // Port B
 	{
-		GPIO_PORTB_DATA_R & pins_mask =  pins_level;
+		if (pins_level==STD_HIGH)
+		{
+			 GPIO_PORTB_DATA_R |= pins_mask;
+		}
+		else if(pins_level==STD_LOW)
+			{
+			GPIO_PORTB_DATA_R &= ~pins_mask;
+		  }
 	}
 	else if (port_index == 2) // Port C
 	{
-		GPIO_PORTC_DATA_R & pins_mask =  pins_level;
+		if (pins_level==STD_HIGH)
+		{
+			 GPIO_PORTC_DATA_R |= pins_mask;
+		}
+		else if(pins_level==STD_LOW)
+			{
+			GPIO_PORTC_DATA_R &= ~pins_mask;
+		  }
 	}
 	else if (port_index == 3) // Port D
 	{
-		GPIO_PORTD_DATA_R & pins_mask =  pins_level;
+		if (pins_level==STD_HIGH)
+		{
+			 GPIO_PORTD_DATA_R |= pins_mask;
+		}
+		else if(pins_level==STD_LOW)
+			{
+			GPIO_PORTD_DATA_R &= ~pins_mask;
+		  }
 	}
 	else if (port_index == 4) // Port E
 	{
-		GPIO_PORTE_DATA_R & pins_mask =  pins_level;
+		if (pins_level==STD_HIGH)
+		{
+			 GPIO_PORTE_DATA_R |= pins_mask;
+		}
+		else if(pins_level==STD_LOW)
+			{
+			GPIO_PORTE_DATA_R &= ~pins_mask;
+		  }
 	}
 	else if (port_index == 5) // Port f
 	{
-		GPIO_PORTF_DATA_R & pins_mask =  pins_level;
+		if (pins_level==STD_HIGH)
+		{
+			 GPIO_PORTF_DATA_R |= pins_mask;
+		}
+		else if(pins_level==STD_LOW)
+			{
+			GPIO_PORTF_DATA_R &= ~pins_mask;
+		  }
 	}
 }
