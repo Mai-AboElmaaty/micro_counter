2ad2a50f9ce9ae6f2079caa9bb8ab589d8cb1014
MazenRefaat23 <36542052+MazenRefaat23@users.noreply.github.com>
2019-03-23 13:59:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -13,17 +13,23 @@ void delay (int n){
 }
 
 void pin_writer (uint8_t count1, uint8_t count2, uint8_t count3) {
+	uint8_t dummy = 0x0F;
+	uint8_t b = ~count1;
+	uint8_t d = ~count2;
+	uint8_t e = ~count3;
 	DIO_WritePort(1, count1 , STD_HIGH);  //output pins for 1st 7 segmant [port A]
 	DIO_WritePort(3, count2 , STD_HIGH);  //output pins for 2nd 7 segmant [port B]
 	DIO_WritePort(4, count3 , STD_HIGH);  //output pins for 3rd 7 segmant [port D]
-	DIO_WritePort(1, ~count1 , STD_LOW);  //output pins for 1st 7 segmant [port A]
-	DIO_WritePort(3, ~count2 , STD_LOW);  //output pins for 2nd 7 segmant [port B]
-	DIO_WritePort(4, ~count3 , STD_LOW);  //output pins for 3rd 7 segmant [port D]
+	DIO_WritePort(1, b , STD_LOW);  //output pins for 1st 7 segmant [port A]
+	DIO_WritePort(3, d , STD_LOW);  //output pins for 2nd 7 segmant [port B]
+	DIO_WritePort(4, e , STD_LOW);  //output pins for 3rd 7 segmant [port D]
 }
 
 
 	
 void initial() {
+	
+	Port_Init(0);
 
 	Port_Init(1);  //init for port B
 	Port_Init(3);  //init for port D
@@ -33,9 +39,11 @@ void initial() {
 	Port_SetPinDirection (3, 0x0F, PORT_PIN_OUT);  //pin 0 1 set output for 1st 7 segmant [port B] pin 4 5 6 7 for 2nd 7 segment 
 	Port_SetPinDirection (4, 0x0F, PORT_PIN_OUT);  //pin 0 1 2 3 set output for 3rd 7 segmant [port D]
 
-	Port_SetPinDirection (1, 0x70, PORT_PIN_IN);	
+	Port_SetPinDirection (1, 0x30, PORT_PIN_IN);	
+	Port_SetPinDirection (0, 0x40, PORT_PIN_IN);
 	
-	Port_SetPinPullUp (1, 0x70, 1);
+	Port_SetPinPullUp (1, 0x30, 1);
+	Port_SetPinPullUp (0, 0x40, 1);
 	
 	
 
@@ -59,13 +67,13 @@ void initial() {
 		count_2 = 0;
 		count_3 = 0;
 		
-pin_writer (count_1, count_2, count_3);
+		pin_writer (count_1, count_2, count_3);
 
 	
 		
 		while(1)
 		{
-			reset = DIO_ReadPort(1, 0x40);
+			reset = DIO_ReadPort(0, 0x40);
 			delay(20);
 			
 			button_up = DIO_ReadPort(1, 0x10);
@@ -73,12 +81,12 @@ pin_writer (count_1, count_2, count_3);
 			button_down = DIO_ReadPort(1, 0x20);
 			delay(20);
 			
-			if (reset == 0){
+		if (reset == 0){
 				count_1 = 0;
 				count_2 = 0;
 				count_3 = 0;
+				while (reset == 0){reset = DIO_ReadPort(0, 0x40);delay(20);}
 				pin_writer (count_1, count_2, count_3);	
-				while (reset == 0){reset = DIO_ReadPort(1, 0x40);delay(20);}
 			}
 			
 			else {
@@ -86,17 +94,24 @@ pin_writer (count_1, count_2, count_3);
 			 if (button_up == 0){
 				if (count_1==9){
 					if(count_2==9) {
-						if(count_3==9) {count_1=0; count_2=0; count_3=0;}
+						if(count_3==9) {
+							count_1=0;
+							count_2=0; 
+							count_3=0;
+							pin_writer (count_1, count_2, count_3);
+							delay(200);
+						}
 						else {
 							count_3++;
 							count_2=0;
+							count_1=0;
 							pin_writer (count_1, count_2, count_3);
 							delay(200);
 						}
 					}
 					else {
-						count_2++;
 						count_1=0;
+						count_2++;
 						pin_writer (count_1, count_2, count_3);
 						delay(200);
 					}
@@ -113,10 +128,16 @@ pin_writer (count_1, count_2, count_3);
 				
 				if (count_1==0){
 						if (count_2==0){
-							if (count_3==0) {count_3=9;count_2=9;count_1=9;}
+							if (count_3==0) {
+								count_3=9;
+								count_2=9;
+								count_1=9;
+								pin_writer (count_1, count_2, count_3);
+							}
 							else { 
 										count_3--;
 										count_2=9;
+										count_1=9;
 										pin_writer (count_1, count_2, count_3);								
 							}
 						}
@@ -134,6 +155,6 @@ pin_writer (count_1, count_2, count_3);
 			}
 		}
 			
-		}
+}
 		
 	}
